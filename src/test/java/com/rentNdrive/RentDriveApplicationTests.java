f89c8c2adf78bb41c1694d7ef306a1f79a05b2c1
MayankJain9910 <mayankj9910@gmail.com>
2025-09-05 23:16:35 +0530

    Initial Commit

---
+++ src/test/java/com/rentNdrive/RentDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rentNdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
